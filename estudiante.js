b19256e7d4ef6acc883d4bdeb385f59f0a321270
xenonpolaris <xeonpolaris2@gmail.com>
2025-07-28 05:39:43 -0400

    Update estudiante.js

--- estudiante.js
+++ estudiante.js
@@ -1,6 +1,6 @@
-const client = new Appwrite.Client()
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('688733b70004aba4f8f1');
+//const client = new Appwrite.Client()
+  //.setEndpoint('https://cloud.appwrite.io/v1')
+  //.setProject('688733b70004aba4f8f1');
 
 const database = new Appwrite.Databases(client);
 const DB_ID = '688733b70004aba4f8f1';
